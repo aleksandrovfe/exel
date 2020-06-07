90a25d4c30a090624570dabeabde4478eb043085
aleksandrovfe <aleksandrov7296@gmail.com>
2020-06-07 22:40:41 +0300

    add resize logic

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,35 +1,40 @@
-import {capitalize} from '@core/utils';
+import {capitalize} from '@core/utils'
 
 export class DomListener {
-    constructor($root, listeners = []) {
-        if (!$root) {
-            new Error('No $root provided DOM Listener')
-        }
-
-        this.$root = $root
-        this.listeners = listeners
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error(`No $root provided for DomListener!`)
     }
+    this.$root = $root
+    this.listeners = listeners
+  }
 
-    initDOMListener() {
-        this.listeners.forEach((listener) => {
-            const method = getMethodName(listener)
-            this[method] = this[method].bind(this)
-            if (!this[method]) {
-                new Error(`Method ${method} is not implement in ${this.name || ''} component`)
-            } else {
-                this.$root.on(listener, this[method])
-            }
-        })
-    }
+  initDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      if (!this[method]) {
+        const name = this.name || ''
+        throw new Error(
+            `Method ${method} is not implemented in ${name} Component`
+        )
+      }
+      this[method] = this[method].bind(this)
+      // Тоже самое что и addEventListener
+      this.$root.on(listener, this[method])
+    })
+  }
 
-    removeDOMListener() {
-        this.listeners.forEach((listener) => {
-            const method = getMethodName(listener)
-            this.$root.off(listener, this[method])
-        })
-    }
+  removeDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      this.$root.off(listener, this[method])
+    })
+  }
 }
 
-const getMethodName = (eventName) => {
-    return `on${capitalize((eventName))}`
+// input => onInput
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
 }
+
+
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,20 +1,21 @@
 import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
-    constructor($root, options = {}) {
-        super($root, options.listeners);
-        this.name = options.name || ''
-    }
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+    this.name = options.name || ''
+  }
 
-    toHtml() {
-        return ''
-    }
+  // Возвращает шаблон компонента
+  toHTML() {
+    return ''
+  }
 
-    init() {
-        this.initDOMListener()
-    }
+  init() {
+    this.initDOMListeners()
+  }
 
-    destroy() {
-        this.removeDOMListener()
-    }
+  destroy() {
+    this.removeDOMListeners()
+  }
 }
